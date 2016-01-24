9a361acd8b9ce7aa29ae579e99dd88aa232f2ef5
Finb <fin.uuid@gmail.com>
2016-01-24 20:42:06 +0800

    add V2EXSettings

---
+++ Common/V2EXSettings.swift
@@ -0,0 +1,28 @@
+//
+//  V2EXSettings.swift
+//  V2ex-Swift
+//
+//  Created by huangfeng on 1/24/16.
+//  Copyright © 2016 Fin. All rights reserved.
+//
+
+import UIKit
+
+let keyPrefix =  "me.fin.V2EXSettings."
+
+class V2EXSettings: NSObject {
+    static let sharedInstance = V2EXSettings()
+    
+    subscript(key:String) -> String? {
+        get {
+            return NSUserDefaults.standardUserDefaults().objectForKey(keyPrefix + key) as? String
+        }
+        set {
+            NSUserDefaults.standardUserDefaults().setValue(newValue, forKey: keyPrefix + key )
+        }
+    }
+    
+    func save(){
+        NSUserDefaults.standardUserDefaults().synchronize()
+    }
+}
