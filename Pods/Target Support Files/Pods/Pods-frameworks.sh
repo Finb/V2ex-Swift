9e7a23a6f967b8661d45cbbf6c03bad0dcc51b93
Finb <fin.uuid@gmail.com>
2016-01-26 13:23:27 +0800

    add SVProgressHUD

--- Pods/Target Support Files/Pods/Pods-frameworks.sh
+++ Pods/Target Support Files/Pods/Pods-frameworks.sh
@@ -92,6 +92,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods/KVOController.framework"
   install_framework "Pods/Kingfisher.framework"
   install_framework "Pods/ObjectMapper.framework"
+  install_framework "Pods/SVProgressHUD.framework"
   install_framework "Pods/SnapKit.framework"
   install_framework "Pods/YYText.framework"
 fi
@@ -104,6 +105,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods/KVOController.framework"
   install_framework "Pods/Kingfisher.framework"
   install_framework "Pods/ObjectMapper.framework"
+  install_framework "Pods/SVProgressHUD.framework"
   install_framework "Pods/SnapKit.framework"
   install_framework "Pods/YYText.framework"
 fi
