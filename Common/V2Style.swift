4b46b9c4ecaf24f14da5d6a0c47ca8146b2f994c
Finb <fin.uuid@gmail.com>
2016-03-10 16:22:33 +0800

    css 功能分离

--- Common/V2Style.swift
+++ Common/V2Style.swift
@@ -9,17 +9,21 @@
 import UIKit
 
 
-//CSS样式字符串
+//CSS基本样式
 private let BASE_CSS = try! String(contentsOfFile: NSBundle.mainBundle().pathForResource("baseStyle", ofType: "css")!, encoding: NSUTF8StringEncoding)
+//文字大小
+private let FONT_CSS = try! String(contentsOfFile: NSBundle.mainBundle().pathForResource("font", ofType: "css")!, encoding: NSUTF8StringEncoding)
+//暗色主题配色
 private let DARK_CSS = (try! String(contentsOfFile: NSBundle.mainBundle().pathForResource("darkStyle", ofType: "css")!, encoding: NSUTF8StringEncoding))
+//亮色主题配色
 private let LIGHT_CSS = (try! String(contentsOfFile: NSBundle.mainBundle().pathForResource("lightStyle", ofType: "css")!, encoding: NSUTF8StringEncoding))
 
 /// 自动维护APP的CSS文件 ,外界只需调用 V2Style.sharedInstance.CSS 即可取得APP所需要的CSS
 class V2Style: NSObject {
     static let sharedInstance = V2Style()
     
-    private var dark_css = BASE_CSS + DARK_CSS
-    private var light_css = BASE_CSS + LIGHT_CSS
+    private var dark_css = BASE_CSS + FONT_CSS + DARK_CSS
+    private var light_css = BASE_CSS + FONT_CSS + LIGHT_CSS
     var CSS = ""
     
     private override init() {
