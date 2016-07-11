e4a3c2c211179fc6b395c457e2b3a36b539b3e82
Finb <fin.uuid@gmail.com>
2016-07-11 16:31:23 +0800

    调整代码

--- View/TopicDetailCommentCell.swift
+++ View/TopicDetailCommentCell.swift
@@ -11,22 +11,60 @@ import YYText
 
 class TopicDetailCommentCell: UITableViewCell{
     /// 头像
-    var avatarImageView: UIImageView?
+    var avatarImageView: UIImageView = {
+        let avatarImageView = UIImageView()
+        avatarImageView.contentMode=UIViewContentMode.ScaleAspectFit
+        avatarImageView.layer.cornerRadius = 3
+        avatarImageView.layer.masksToBounds = true
+        return avatarImageView
+    }()
     /// 用户名
-    var userNameLabel: UILabel?
+    var userNameLabel: UILabel = {
+        let userNameLabel = UILabel()
+        userNameLabel.textColor = V2EXColor.colors.v2_TopicListUserNameColor
+        userNameLabel.font=v2Font(14)
+        return userNameLabel
+    }()
     /// 日期 和 最后发送人
-    var dateLabel: UILabel?
+    var dateLabel: UILabel = {
+        let dateLabel = UILabel()
+        dateLabel.textColor=V2EXColor.colors.v2_TopicListDateColor
+        dateLabel.font=v2Font(12)
+        return dateLabel
+    }()
 
     /// 回复正文
-    var commentLabel: YYLabel?
+    var commentLabel: YYLabel = {
+        let commentLabel = YYLabel();
+        commentLabel.textColor=V2EXColor.colors.v2_TopicListTitleColor;
+        commentLabel.font = v2Font(14);
+        commentLabel.numberOfLines = 0;
+        commentLabel.displaysAsynchronously = true
+        return commentLabel
+    }()
     
     /// 装上面定义的那些元素的容器
-    var contentPanel:UIView?
+    var contentPanel: UIView = {
+        let view = UIView()
+        view.backgroundColor = V2EXColor.colors.v2_CellWhiteBackgroundColor
+        return view
+    }()
     
     //评论喜欢数
-    var favoriteIconView:UIImageView?
-    var favoriteLabel:UILabel?
-    
+    var favoriteIconView:UIImageView = {
+        let favoriteIconView = UIImageView(image: UIImage.imageUsedTemplateMode("ic_favorite_18pt")!)
+        favoriteIconView.tintColor = V2EXColor.colors.v2_TopicListDateColor;
+        favoriteIconView.contentMode = .ScaleAspectFit
+        favoriteIconView.hidden = true
+        return favoriteIconView
+    }()
+
+    var favoriteLabel:UILabel = {
+        let favoriteLabel = UILabel()
+        favoriteLabel.textColor = V2EXColor.colors.v2_TopicListDateColor;
+        favoriteLabel.font = v2Font(10)
+        return favoriteLabel
+    }()
     weak var itemModel:TopicCommentModel?
     
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
@@ -36,99 +74,38 @@ class TopicDetailCommentCell: UITableViewCell{
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
+
     func setup()->Void{
         self.backgroundColor=V2EXColor.colors.v2_backgroundColor;
         
         let selectedBackgroundView = UIView()
         selectedBackgroundView.backgroundColor = V2EXColor.colors.v2_backgroundColor
         self.selectedBackgroundView = selectedBackgroundView
+
+        self.contentView.addSubview(self.contentPanel);
+        self.contentPanel.addSubview(self.avatarImageView);
+        self.contentPanel .addSubview(self.userNameLabel);
+        self.contentPanel.addSubview(self.favoriteIconView)
+        self.contentPanel.addSubview(self.favoriteLabel)
+        self.contentPanel.addSubview(self.dateLabel);
+        self.contentPanel.addSubview(self.commentLabel);
+
+        self.setupLayout()
         
-        self.contentPanel = UIView();
-        self.contentPanel!.backgroundColor = V2EXColor.colors.v2_CellWhiteBackgroundColor
-        self.contentView .addSubview(self.contentPanel!);
-        self.contentPanel!.snp_makeConstraints{ (make) -> Void in
-            make.top.left.right.equalTo(self.contentView);
-        }
-        
-        self.avatarImageView = UIImageView();
-        self.avatarImageView!.contentMode=UIViewContentMode.ScaleAspectFit;
-        self.avatarImageView!.layer.cornerRadius = 3;
-        self.avatarImageView!.layer.masksToBounds = true;
-        self.contentPanel!.addSubview(self.avatarImageView!);
-        self.avatarImageView!.snp_makeConstraints{ (make) -> Void in
-            make.left.top.equalTo(self.contentView).offset(12);
-            make.width.height.equalTo(35);
-        }
-        
-        self.userNameLabel = UILabel();
-        self.userNameLabel!.textColor = V2EXColor.colors.v2_TopicListUserNameColor;
-        self.userNameLabel!.font=v2Font(14);
-        self.contentPanel! .addSubview(self.userNameLabel!);
-        self.userNameLabel!.snp_makeConstraints{ (make) -> Void in
-            make.left.equalTo(self.avatarImageView!.snp_right).offset(10);
-            make.top.equalTo(self.avatarImageView!);
-        }
-        
-        self.favoriteIconView = UIImageView(image: UIImage.imageUsedTemplateMode("ic_favorite_18pt")!)
-        self.favoriteIconView?.tintColor = V2EXColor.colors.v2_TopicListDateColor;
-        self.favoriteIconView?.contentMode = .ScaleAspectFit
-        self.contentPanel?.addSubview(self.favoriteIconView!)
-        self.favoriteIconView!.snp_makeConstraints{ (make) -> Void in
-            make.centerY.equalTo(self.userNameLabel!);
-            make.left.equalTo(self.userNameLabel!.snp_right).offset(10)
-            make.width.height.equalTo(10)
-        }
-        self.favoriteIconView!.hidden = true
-        
-        self.favoriteLabel = UILabel()
-        self.favoriteLabel!.textColor = V2EXColor.colors.v2_TopicListDateColor;
-        self.favoriteLabel!.font = v2Font(10)
-        self.contentPanel!.addSubview(self.favoriteLabel!)
-        self.favoriteLabel!.snp_makeConstraints{ (make) -> Void in
-            make.left.equalTo(self.favoriteIconView!.snp_right).offset(3)
-            make.centerY.equalTo(self.favoriteIconView!)
-        }
-        
-        self.dateLabel = UILabel();
-        self.dateLabel!.textColor=V2EXColor.colors.v2_TopicListDateColor;
-        self.dateLabel!.font=v2Font(12);
-        self.contentPanel?.addSubview(self.dateLabel!);
-        self.dateLabel!.snp_makeConstraints{ (make) -> Void in
-            make.bottom.equalTo(self.avatarImageView!);
-            make.left.equalTo(self.userNameLabel!);
-        }
-        
-        self.commentLabel = YYLabel();
-        self.commentLabel!.textColor=V2EXColor.colors.v2_TopicListTitleColor;
-        self.commentLabel!.font = v2Font(14);
-        self.commentLabel!.numberOfLines = 0;
-        self.commentLabel!.displaysAsynchronously = true
-        self.contentPanel?.addSubview(self.commentLabel!);
-        self.commentLabel!.snp_makeConstraints{ (make) -> Void in
-            make.top.equalTo(self.avatarImageView!.snp_bottom).offset(12);
-            make.left.equalTo(self.avatarImageView!);
-            make.right.equalTo(self.contentPanel!).offset(-12);
-            make.bottom.equalTo(self.contentPanel!.snp_bottom).offset(-12)
-        }
-        
-        self.contentPanel!.snp_makeConstraints{ (make) -> Void in
-            make.bottom.equalTo(self.contentView.snp_bottom).offset(-SEPARATOR_HEIGHT);
-        }
-        
-        self.avatarImageView!.backgroundColor = self.contentPanel!.backgroundColor
-        self.userNameLabel!.backgroundColor = self.contentPanel!.backgroundColor
-        self.dateLabel!.backgroundColor = self.contentPanel!.backgroundColor
-        self.commentLabel!.backgroundColor = self.contentPanel!.backgroundColor
-        self.favoriteIconView!.backgroundColor = self.contentPanel!.backgroundColor
-        self.favoriteLabel!.backgroundColor = self.contentPanel!.backgroundColor
+        self.avatarImageView.backgroundColor = self.contentPanel.backgroundColor
+        self.userNameLabel.backgroundColor = self.contentPanel.backgroundColor
+        self.dateLabel.backgroundColor = self.contentPanel.backgroundColor
+        self.commentLabel.backgroundColor = self.contentPanel.backgroundColor
+        self.favoriteIconView.backgroundColor = self.contentPanel.backgroundColor
+        self.favoriteLabel.backgroundColor = self.contentPanel.backgroundColor
         
         //点击用户头像，跳转到用户主页
-        self.avatarImageView!.userInteractionEnabled = true
-        self.userNameLabel!.userInteractionEnabled = true
+        self.avatarImageView.userInteractionEnabled = true
+        self.userNameLabel.userInteractionEnabled = true
         var userNameTap = UITapGestureRecognizer(target: self, action: #selector(TopicDetailCommentCell.userNameTap(_:)))
-        self.avatarImageView!.addGestureRecognizer(userNameTap)
+        self.avatarImageView.addGestureRecognizer(userNameTap)
         userNameTap = UITapGestureRecognizer(target: self, action: #selector(TopicDetailCommentCell.userNameTap(_:)))
-        self.userNameLabel!.addGestureRecognizer(userNameTap)
+        self.userNameLabel.addGestureRecognizer(userNameTap)
         
         //长按手势
         self.contentView .addGestureRecognizer(
@@ -136,6 +113,42 @@ class TopicDetailCommentCell: UITableViewCell{
                 action: #selector(TopicDetailCommentCell.longPressHandle(_:))
             )
         )
+    }
+    func setupLayout(){
+        self.contentPanel.snp_makeConstraints{ (make) -> Void in
+            make.top.left.right.equalTo(self.contentView);
+        }
+        self.avatarImageView.snp_makeConstraints{ (make) -> Void in
+            make.left.top.equalTo(self.contentView).offset(12);
+            make.width.height.equalTo(35);
+        }
+        self.userNameLabel.snp_makeConstraints{ (make) -> Void in
+            make.left.equalTo(self.avatarImageView.snp_right).offset(10);
+            make.top.equalTo(self.avatarImageView);
+        }
+        self.favoriteIconView.snp_makeConstraints{ (make) -> Void in
+            make.centerY.equalTo(self.userNameLabel);
+            make.left.equalTo(self.userNameLabel.snp_right).offset(10)
+            make.width.height.equalTo(10)
+        }
+        self.favoriteLabel.snp_makeConstraints{ (make) -> Void in
+            make.left.equalTo(self.favoriteIconView.snp_right).offset(3)
+            make.centerY.equalTo(self.favoriteIconView)
+        }
+        self.dateLabel.snp_makeConstraints{ (make) -> Void in
+            make.bottom.equalTo(self.avatarImageView);
+            make.left.equalTo(self.userNameLabel);
+        }
+        self.commentLabel.snp_makeConstraints{ (make) -> Void in
+            make.top.equalTo(self.avatarImageView.snp_bottom).offset(12);
+            make.left.equalTo(self.avatarImageView);
+            make.right.equalTo(self.contentPanel).offset(-12);
+            make.bottom.equalTo(self.contentPanel.snp_bottom).offset(-12)
+        }
+        
+        self.contentPanel.snp_makeConstraints{ (make) -> Void in
+            make.bottom.equalTo(self.contentView.snp_bottom).offset(-SEPARATOR_HEIGHT);
+        }
     }
     func userNameTap(sender:UITapGestureRecognizer) {
         if let _ = self.itemModel , username = itemModel?.userName {
@@ -147,15 +160,15 @@ class TopicDetailCommentCell: UITableViewCell{
     func bind(model:TopicCommentModel){
         
         
-        self.userNameLabel?.text = model.userName;
-        self.dateLabel?.text = String(format: "%i楼  %@", model.number, model.date ?? "")
+        self.userNameLabel.text = model.userName;
+        self.dateLabel.text = String(format: "%i楼  %@", model.number, model.date ?? "")
 
         
         if model != self.itemModel {
             //如果新旧model相同,则不需要赋值
             //不然layout需要重新绘制，会造成刷新闪烁
             if let layout = model.textLayout {
-                self.commentLabel?.textLayout = layout
+                self.commentLabel.textLayout = layout
                 if layout.attachments != nil {
                     for attachment in layout.attachments {
                         if let attachment = attachment as? YYTextAttachment ,  let image = attachment.content as? V2CommentAttachmentImage{
@@ -167,11 +180,11 @@ class TopicDetailCommentCell: UITableViewCell{
         }
         
         if let avata = model.avata {
-            self.avatarImageView?.fin_setImageWithUrl(NSURL(string: "https:" + avata)!, placeholderImage: nil, imageModificationClosure: fin_defaultImageModification())
+            self.avatarImageView.fin_setImageWithUrl(NSURL(string: "https:" + avata)!, placeholderImage: nil, imageModificationClosure: fin_defaultImageModification())
         }
         
-        self.favoriteIconView!.hidden = model.favorites <= 0
-        self.favoriteLabel!.text = model.favorites <= 0 ? "" : "\(model.favorites)"
+        self.favoriteIconView.hidden = model.favorites <= 0
+        self.favoriteLabel.text = model.favorites <= 0 ? "" : "\(model.favorites)"
         
         self.itemModel = model
     }
